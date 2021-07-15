e2f7cea9d23f286e716c52c00b76e69f3dd3b6f8
Navi-23 <ivan_23.04.1998@mail.ru>
2021-07-15 13:30:15 +0400

    Обновил сайт
    Добавил страницу регистрации

--- SurfClub/Controllers/RegisterController.cs
+++ SurfClub/Controllers/RegisterController.cs
@@ -1,16 +1,100 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SurfClub.Helpers;
+using SurfClub.Models;
+using SurfClub.Models.dbModels;
+using SurfClub.Models.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace SurfClub.Controllers
 {
     public class RegisterController : Controller
     {
+        private readonly SurfClubDBContext dbContext;
+        public RegisterController(SurfClubDBContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+
         public IActionResult Index()
         {
             return View();
         }
+        [HttpPost]
+        public IActionResult Register(User model, IFormFile imageData)
+        {
+
+            if (ModelState.IsValid)
+            {
+                ///проверять, что пользователь есть в БД
+                var user = dbContext.Users.FirstOrDefault(c =>
+                c.Nickname == model.Nickname);
+
+                //Прописать условие
+                if (user != null)
+                {
+                    ModelState.AddModelError(string.Empty, "Такой пользователь уже зарегестрирован");
+                    return View("Index", model);
+                }
+                if (model.Password != model.ConfirmPassword)
+                {
+                    ModelState.AddModelError(string.Empty, "Пароли не совпадают");
+                    return View("Index", model);
+                }
+                else
+                {
+                    model.Photo = ImageHelper.UploadImage(imageData);
+
+                    // авторизовать на сайте, что-то сделать
+                    dbContext.Users.Add(model);
+                    dbContext.SaveChanges();
+
+
+
+                    var claims = new List<Claim>
+            {
+
+
+
+                new Claim(ClaimsIdentity.DefaultNameClaimType, model.Id.ToString())
+            };
+                    var authProp = new AuthenticationProperties();
+
+                 
+
+                    // создаем объект ClaimsIdentity
+                    ClaimsIdentity id = new ClaimsIdentity(claims, "ApplicationCookie", ClaimsIdentity.DefaultNameClaimType, ClaimsIdentity.DefaultRoleClaimType);
+                    // установка аутентификационных куки
+                    HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,
+                        new ClaimsPrincipal(id), authProp);
+
+                    HttpContext.Session.SetString("NickName", model.Nickname);
+                    HttpContext.Session.SetString("Photo", model.Photo.ToString());
+                    HttpContext.Session.SetInt32("UserId", model.Id);
+
+                    return RedirectToAction("Index", "Feed");
+                }
+
+
+            }
+            return View("Index", model);
+        }
+
+
+        public IActionResult Logout()
+        {
+            HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            HttpContext.Session.Clear();
+            return RedirectToAction("Index", "Feed");
+        }
+
+
     }
 }
--- SurfClub/Models/DBModels/User.cs
+++ SurfClub/Models/DBModels/User.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,14 +10,36 @@ namespace SurfClub.Models.dbModels
     public class User
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Псевдоним обязательный")]
+        [MaxLength(20), MinLength(3, ErrorMessage = "Минимимальная длина псевдонима 3 символа")]
         public String Nickname { get; set; }
+
+        [MaxLength(31, ErrorMessage = "Максимальная длина пароля 31 символ")]
+        [EmailAddress(ErrorMessage ="Почта не шаблонного вида")]
         public String Email { get; set; }
+
+        [Required(ErrorMessage = "Пароль обязательный")]
+        [MaxLength(20), MinLength(3, ErrorMessage = "Минимимальная длина пароля 3 символа")]
         public String Password { get; set; }
+
+        [NotMapped]
+        public String ConfirmPassword { get; set; }
+
+        [MaxLength(31, ErrorMessage = "Максимальная длина пароля 31 символ")]
         public String Surname { get; set; }
+
+        [MaxLength(31, ErrorMessage = "Максимальная длина пароля 31 символ")]
         public String Name { get; set; }
+
+        [MaxLength(255, ErrorMessage = "Максимальная длина пароля 255 символ")]
         public String Contact { get; set; }
+
+        [MaxLength(255, ErrorMessage = "Максимальная длина пароля 255 символ")]
         public String AboutMe { get; set; }
+
+        [MaxLength(255, ErrorMessage = "Максимальная длина пароля 255 символ")]
         public String Progress { get; set; }
-        public Guid Photo { get; set; }
+        public Guid? Photo { get; set; }
     }
 }
